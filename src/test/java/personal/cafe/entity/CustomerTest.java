f14d95ce2e50a32914fdb71b3ec3d890ef6e00d1
Sung Hyobin <hellmir610@gmail.com>
2023-07-27 07:59:35 +0900

    test(Customer Test): Menu 객체 생성과 validateCoffeeInputFormat 메서드 호출 위치 변경에 따라 CustomerTest 클래스의 테스트 목록 수정

--- src/test/java/personal/cafe/entity/CustomerTest.java
+++ src/test/java/personal/cafe/entity/CustomerTest.java
@@ -1,117 +1,108 @@
 package personal.cafe.entity;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import personal.cafe.constant.CoffeeName;
 import personal.cafe.constant.CoffeeSize;
 import personal.cafe.exception.InsufficientBalanceException;
-import personal.cafe.validation.CoffeeInputFormatValidator;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class CustomerTest {
 
-    @ParameterizedTest
-    @CsvSource({
-            "10_000, 바리스타1, AMERICANO, TALL, ICE",
-            "20_000, 바리스타2, CAFFE_LATTE, GRANDE, HOT",
-            "30_000, 바리스타3, AMERICANO, VENTI, ICE"
-    })
-    void validateOrderCoffeeIsValid(Integer money, Barista barista, String coffeeName, String coffeeSize, String iceOption) {
 
-        Customer customer = new Customer("지갑", Wallet.createWallet());
-
-        assertDoesNotThrow(() -> customer.orderCoffee(money, barista, coffeeName, coffeeSize, iceOption));
-
-    }
-
+    @DisplayName("고객이 유효하지 않은 금액을 가지고 커피를 주문하려 하면 IllegalArgumentException이 발생한다.")
     @Test
     void validateOrderCoffeeHasInValidChargingMoney() {
 
+        // given
         Customer customer = new Customer("지갑", Wallet.createWallet());
 
+        Menu menu = Menu.createMenu
+                (CoffeeName.valueOf("AMERICANO"), CoffeeSize.valueOf("TALL"), true);
+
+        // when-then
         assertThatThrownBy(() -> customer.orderCoffee
-                (0, new Barista("바리스타"), "AMERICANO", "TALL", "ICE"))
+                (0, new Barista("바리스타"), menu))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("충전 금액은 0 또는 음수값이 될 수 없습니다.");
 
     }
 
+    @DisplayName("고객이 주문하기에 충분하지 않은 금액을 가지고 커피를 주문하려 하면 InsufficientBalanceException이 발생한다.")
     @Test
     void validateOrderCoffeeHasInValidPayment() {
 
+        // given
         Customer customer = new Customer("지갑", Wallet.createWallet());
 
+        Menu menu = Menu.createMenu
+                (CoffeeName.valueOf("CAFFE_LATTE"), CoffeeSize.valueOf("VENTI"), true);
+
+        // when-then
         assertThatThrownBy(() -> customer.orderCoffee
-                (3_000, new Barista("바리스타"), "AMERICANO", "TALL", "ICE"))
+                (3_000, new Barista("바리스타"), menu))
                 .isInstanceOf(InsufficientBalanceException.class)
                 .hasMessage("잔액이 부족합니다. 결제하시려면 금액을 더 충전해 주세요. (현재 잔액: "
                         + customer.getWallet().getChargedMoney() + "원, 주문 금액: "
-                        + (int) (CoffeeName.valueOf("AMERICANO").getPrice()
-                        + CoffeeSize.valueOf("TALL").getPrice()) + "원)");
+                        + (int) (CoffeeName.valueOf("CAFFE_LATTE").getPrice()
+                        + CoffeeSize.valueOf("VENTI").getPrice()) + "원)");
 
     }
 
-    @Test
-    void validateOrderCoffeeHasInValidCoffeeFormat() {
-
-        Customer customer = new Customer("지갑", Wallet.createWallet());
-
-        CoffeeInputFormatValidator coffeeInputFormatValidator = mock(CoffeeInputFormatValidator.class);
-
-        List<String> expectedOptions = Arrays.asList("AMERICANO", "CAFFE_LATTE");
-
-        when(coffeeInputFormatValidator.getCoffeeOptionList()).thenReturn(expectedOptions);
-
-        assertThatThrownBy(() -> customer.orderCoffee
-                (30_000, new Barista("바리스타"), "아메리카노", "톨", "아이스"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("존재하지 않는 커피 이름입니다. 현재 주문 가능한 커피 이름 목록: "
-                        + coffeeInputFormatValidator.getCoffeeOptionList());
-
-    }
-
-
+    @DisplayName("커피를 주문한 후 고객의 지갑에는 '충전 금액 - 주문 가격'만큼의 잔액이 남아 있다.")
     @ParameterizedTest
     @CsvSource({
             "10_000, 바리스타1, AMERICANO, TALL, ICE",
             "20_000, 바리스타2, CAFFE_LATTE, GRANDE, HOT",
             "30_000, 바리스타3, AMERICANO, VENTI, ICE"
     })
-    void checkWalletBalanceAfterOrderCoffee(Integer money, Barista barista, String coffeeName, String coffeeSize, String iceOption) {
+    void checkWalletBalanceAfterOrderCoffee
+            (Integer money, Barista barista, String coffeeName, String coffeeSize, String iceOption) {
 
+        // given
         Customer customer = new Customer("지갑", Wallet.createWallet());
 
-        customer.orderCoffee(money, barista, coffeeName, coffeeSize, iceOption);
+        Menu menu = Menu.createMenu
+                (CoffeeName.valueOf(coffeeName), CoffeeSize.valueOf(coffeeSize), iceOption.equals("ICE"));
 
+        // when
+        customer.orderCoffee(money, barista, menu);
+
+        // then
         assertThat(customer.getWallet().getChargedMoney())
-                .isEqualTo(+money - (CoffeeName.valueOf(coffeeName).getPrice()
-                        + CoffeeSize.valueOf(coffeeSize).getPrice()));
+                .isEqualTo(+money - (menu.getCoffeeName().getPrice()
+                        + menu.getCoffeeSize().getPrice()));
 
     }
 
+    @DisplayName("커피를 주문한 후 고객은 입력된 메뉴판의 커피 종류와 동일한 커피를 가지고 있다.")
     @ParameterizedTest
     @CsvSource({
             "10_000, 바리스타1, AMERICANO, TALL, ICE",
             "20_000, 바리스타2, CAFFE_LATTE, GRANDE, HOT",
             "30_000, 바리스타3, AMERICANO, VENTI, ICE"
     })
-    void checkCoffeeIsCorrectAfterOrderCoffee(Integer money, Barista barista, String coffeeName, String coffeeSize, String iceOption) {
+    void checkCoffeeIsCorrectAfterOrderCoffee
+            (Integer money, Barista barista, String coffeeName, String coffeeSize, String iceOption) {
 
+        // given
         Customer customer = new Customer("지갑", Wallet.createWallet());
 
-        customer.orderCoffee(money, barista, coffeeName, coffeeSize, iceOption);
+        Menu menu = Menu.createMenu
+                (CoffeeName.valueOf(coffeeName), CoffeeSize.valueOf(coffeeSize), iceOption.equals("ICE"));
 
+        // when
+        customer.orderCoffee(money, barista, menu);
+
+        // then
         assertThat(customer.getCoffee().getCoffeeName()).isEqualTo(CoffeeName.valueOf(coffeeName));
+
         assertThat(customer.getCoffee().getCoffeeSize()).isEqualTo(CoffeeSize.valueOf(coffeeSize));
+
         assertThat(customer.getCoffee().isIced()).isEqualTo(iceOption.equals("ICE"));
 
     }
